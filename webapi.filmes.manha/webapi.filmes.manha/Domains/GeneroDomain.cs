7bc41382a0631e945d89a1f38f74592b8f743af6
Mateus <mateusmouraferreira8@gmail.com>
2023-08-21 10:53:23 -0300

    alteracoes

---
+++ webapi.filmes.manha/webapi.filmes.manha/Domains/GeneroDomain.cs
@@ -0,0 +1,15 @@
+﻿namespace webapi.filmes.manha.Domains
+{
+    /// <summary>
+    /// Classe que representa a entidade(tabela) Genero
+    /// </summary>
+    public class GeneroDomain
+    {
+       public int IdGenero { get; set; }
+        public string Name { get; set; }
+
+        
+
+
+    }
+}
---
+++ webapi.filmes.manha/webapi.filmes.manha/Interfaces/IGeneroRepository.cs
@@ -0,0 +1,19 @@
+﻿using webapi.filmes.manha.Domains;
+
+namespace webapi.filmes.manha.Interfaces
+{
+    /// <summary>
+    /// Interface responsavel pelo repositorio GeneroRepository
+    /// Definir os metodos que serao implementados pelo GeneroRepository
+    /// </summary>
+    public interface IGeneroRepository
+    {
+        ///tipoRetorno NomeMetodo(tipoParametro nomeParametro)
+
+        void Cadastrar(GeneroDomain novoGenero);
+        List<GeneroDomain> ListarTodos();
+        void AtualizarIdCorpo(GeneroDomain genero);
+
+        void Deletar(int id);
+    }
+}
