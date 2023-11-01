3704f091b6adbe864d621cce95ed1b6649385452
Mateus <mateusmouraferreira8@gmail.com>
2023-11-01 10:53:05 -0300

    alteracoes

--- REACT/eventplus/src/components/Header/Header.jsx
+++ REACT/eventplus/src/components/Header/Header.jsx
@@ -4,13 +4,16 @@ import './Header.css'
 import Container from "../Container/Container";
 import Nav from "../Nav/Nav";
 import PerfilUsuario from '../PerfilUsuario/PerfilUsuario'
+import menubar from '../../assets/images/menubar.png'
+import logMobile from '../../assets/images/logo-white.svg'
+import logDesktop from '../../assets/images/logo-pink.svg'
 
 const Header = () => {
     return(
         <header className="headerpage">
             <Container>
                 <div className="header-flex">
-                    <img src="" alt="Imagem menu de barras. Serve para exibir ou esconder o menu no smartphone" />
+                    <img src={menubar} alt="Imagem menu de barras. Serve para exibir ou esconder o menu no smartphone" />
 
                     <Nav />
                     <PerfilUsuario />
--- REACT/eventplus/src/components/Nav/Nav.jsx
+++ REACT/eventplus/src/components/Nav/Nav.jsx
@@ -1,10 +1,25 @@
 import React from 'react';
 import './Nav.css'
 
+import logMobile from '../../assets/images/logo-white.svg'
+import logDesktop from '../../assets/images/logo-pink.svg'
+
 const Nav = () => {
     return(
-        <nav>
+        <nav className='navbar'>
+            <span className='navbar__close'>x</span>
 
+            <a href="" className='eventlogo'>
+            <img className='eventlogo__logo-image'
+            src={window.innerWidth >= 992 ? logDesktop : logMobile}
+             alt="Event Plus Logo" />
+            </a>
+
+            <div className='navbar__items-box'>
+                <a href="">Home</a>
+                <a href="">Tipos de Evento</a>
+                <a href="">Usuários</a>
+            </div>
         </nav>
     );
 };
