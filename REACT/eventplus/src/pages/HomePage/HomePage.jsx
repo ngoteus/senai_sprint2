22589e89ded0fc249c0594cd6e18ac5dc734508a
Mateus <mateusmouraferreira8@gmail.com>
2023-11-09 10:56:44 -0300

    alteracoes

--- REACT/eventplus/src/pages/HomePage/HomePage.jsx
+++ REACT/eventplus/src/pages/HomePage/HomePage.jsx
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./HomePage.css"
 import ContactSection from "../../components/ContatcSection/ContactSection";
 import VisionSection from "../../components/VisionSection/VisionSection";
@@ -7,8 +7,26 @@ import MainContent from "../../components/Main/MainContent";
 import Title from "../../components/Titulo/Title";
 import NextEvent from "../../components/NextEvent/NextEvent";
 import Container from "../../components/Container/Container";
+import axios from "axios";
 
 const HomePage = () => {
+
+    const [nextEvents, setNextEvents] = useState([]);
+    const urlLocal = 'https://localhost:7118/api'
+    useEffect(() => {
+      async function getNextEvents(){
+        try {
+            const promise = await axios.get(`${urlLocal}/Evento/ListarProximos`);
+            const dados =  await promise.data;
+
+            setNextEvents(dados);
+        } catch (error) {
+            alert("Deu ruim na api!")
+        }
+      }
+
+      getNextEvents();
+    }, [])
     return(
             <MainContent>
                 <Banner />
@@ -17,9 +35,20 @@ const HomePage = () => {
                     <Title titleText={"Proximos Eventos"} />
 
                     <div className="events-box">
-                        <NextEvent 
-                        title={"Evento X"}/>
-                        <NextEvent />
+
+                        {
+                            nextEvents.map((e) => {
+                            return(
+                            <NextEvent 
+                            key={e.idEvento}
+                        title={e.nomeEvento}
+                        description={e.descricao}
+                        eventDate={e.dataEvento}
+                        idEvent={e.idEvento}/> 
+                            )
+                    })};
+
+                        
                         
                     </div>
                     </Container>
--- REACT/eventplus/src/pages/Teste/TestePage.jsx
+++ REACT/eventplus/src/pages/Teste/TestePage.jsx
@@ -1,63 +1,79 @@
-import React, {useState} from "react";
+import React, { useEffect, useState} from "react";
 
 import Input from "../../components/Input/Input";
 import Button from "../../components/Button/Button";
 
 const TestePage = () => {
-    const [n1, setN1] = useState(0);
-    const [n2, setN2] = useState(0);
-    const [total, setTotal] = useState(0);
 
-    function handleCalcular(e) {
-        e.preventDefault();
-        setTotal(parseFloat(n1) +  parseFloat(n2));
-    }
+    const [count, setCount] = useState(0);
+    const [calculation, setCalculation] = useState(0);
 
-    return (
-        <div>
+    useEffect(() =>{
+        setCalculation( count * 2);
+    }, [count]);
 
-            {/* <Header /> */}
-            <h1>Pagina de poc´s</h1>
-            <h2>Calculator</h2>
+    return(
+        <>
+            <p>Count: {count}</p>
+            <button onClick={() => setCount((c) => c + 1)}>+</button>
+            <p>Calculation: {calculation}</p>
+        </>
+    );
+};
+    // const [n1, setN1] = useState(0);
+    // const [n2, setN2] = useState(0);
+    // const [total, setTotal] = useState(0);
 
-            <form onSubmit={handleCalcular}
+    // function handleCalcular(e) {
+    //     e.preventDefault();
+    //     setTotal(parseFloat(n1) +  parseFloat(n2));
+    // }
+
+    // return (
+    //     <div>
+
+    //         {/* <Header /> */}
+    //         <h1>Pagina de poc´s</h1>
+    //         <h2>Calculator</h2>
+
+    //         <form onSubmit={handleCalcular}
             
-            >
-                <Input 
-                type="number"
-                placeholder="Primeiro numero"
-                name="n1"
-                id="n1" 
-                value={n1}
-                onChange={(e) => {setN1(e.target.value)}}
-                />
-                <br />
-                <Input 
-                type="number"
-                placeholder="segundo numero"
-                name="n2"
-                id="n2" 
-                value={n2}
-                onChange={(e) => {setN2(e.target.value)}}
-                />
+    //         >
+    //             <Input 
+    //             type="number"
+    //             placeholder="Primeiro numero"
+    //             name="n1"
+    //             id="n1" 
+    //             value={n1}
+    //             onChange={(e) => {setN1(e.target.value)}}
+    //             />
+    //             <br />
+    //             <Input 
+    //             type="number"
+    //             placeholder="segundo numero"
+    //             name="n2"
+    //             id="n2" 
+    //             value={n2}
+    //             onChange={(e) => {setN2(e.target.value)}}
+    //             />
 
-                <br />
+    //             <br />
 
-                <Button 
-                    textButton="Calcular"
-                    type="submit"
-                />
-                <span>
-                    Resultado: 
-                    <strong >
-                        {total}
-                    </strong>
-                </span>
-            </form>
-            {/* <p>VALOR DO N1: {n1}</p>
-            <p>VALOR DO N1: {n2}</p> */}
-        </div>
-    )
-}
+    //             <Button 
+    //                 textButton="Calcular"
+    //                 type="submit"
+    //             />
+    //             <span>
+    //                 Resultado: 
+    //                 <strong >
+    //                     {total}
+    //                 </strong>
+    //             </span>
+    //         </form>
+    //         {/* <p>VALOR DO N1: {n1}</p>
+    //         <p>VALOR DO N1: {n2}</p> */}
+    //     </div>
+    // )
+
 
 export default TestePage;
