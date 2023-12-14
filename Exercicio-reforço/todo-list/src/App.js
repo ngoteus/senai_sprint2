afe15a2fb6235b779d7cd6361932542c077cb19c
ngoteus <mateusmouraferreira8@gmail.com>
2023-12-14 19:10:41 -0300

    alteracoes todo list

---
+++ Exercicio-reforço/todo-list/src/App.js
@@ -0,0 +1,28 @@
+// import logo from './logo.svg';
+// import './App.css';
+// import Form from './components/Form';
+// import TodoItem from './components/todoItem';
+// import { Container, List, colors } from '@mui/material';
+
+// function App() {
+//   return (
+//     <Container maxWidth="xs" style={{ marginTop: "1em" }}>
+//       <Form />
+//       <List  sx={{bgcolor: 'background.paper', marginTop: "1em" }}>
+//       <TodoItem />
+//       </List>
+      
+//     </Container>
+//   );
+// }
+
+// export default App;
+
+import "./App.css";
+import Home from "./pages/Home";
+
+function App() {
+  return <Home />;
+}
+
+export default App;
